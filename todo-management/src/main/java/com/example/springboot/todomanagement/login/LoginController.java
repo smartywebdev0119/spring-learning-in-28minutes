d0cb3e2e4d5de1528fa5eb9a21be2f8bfc8a76ce
smartywebdev0119 <smartwebdev0119@gmail.com>
2023-06-18 09:08:25 +0530

    implementing logging SLF4j

--- todo-management/src/main/java/com/example/springboot/todomanagement/login/LoginController.java
+++ todo-management/src/main/java/com/example/springboot/todomanagement/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.example.springboot.todomanagement.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
     // http:localhost:8080/login?name=something
     @RequestMapping("login")
     public String loginPage(@RequestParam String name, @RequestParam String username, @RequestParam String password,
@@ -15,6 +19,10 @@ public class LoginController {
         model.put("name", name);
         model.put("username", username);
         model.put("password", password);
+
+        logger.debug("First request param is {}", name);
+        logger.debug("Second request param is {}", username);
+        logger.debug("Third request param is {}", password);
         return "login";
     }
 }
