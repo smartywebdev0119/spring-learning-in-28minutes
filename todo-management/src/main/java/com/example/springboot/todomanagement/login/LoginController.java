ac498867a654890871d4df1bc607b6cce13cbd23
smartywebdev0119 <smartwebdev0119@gmail.com>
2023-06-18 11:22:30 +0530

    redirect login page to welcome page

--- todo-management/src/main/java/com/example/springboot/todomanagement/login/LoginController.java
+++ todo-management/src/main/java/com/example/springboot/todomanagement/login/LoginController.java
@@ -1,14 +1,26 @@
 package com.example.springboot.todomanagement.login;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
 
     // http:localhost:8080/login?name=something
-    @RequestMapping("login")
+    @RequestMapping(value = "login", method = RequestMethod.GET)
     public String loginPage() {
         return "login";
+    }
+
+    @RequestMapping(value = "login", method = RequestMethod.POST)
+    public String welcomePage(@RequestParam String name,
+                              @RequestParam String password,
+                              ModelMap model){
+        model.put("name", name);
+        model.put("password", password);
+        return "welcome";
     }
 }
---
+++ todo-management/src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
@@ -0,0 +1,9 @@
+<html>
+<head>
+<title>Login</title>
+</head>
+<body>
+<h1>Welcome to todo management</h1>
+${name} and ${password}
+</body>
+</html>
