ff361208421926ab851db4899b66501c58128869
smartywebdev0119 <smartwebdev0119@gmail.com>
2023-06-18 11:38:32 +0530

    Authentication username and password

--- todo-management/src/main/java/com/example/springboot/todomanagement/login/LoginController.java
+++ todo-management/src/main/java/com/example/springboot/todomanagement/login/LoginController.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+    private AuthenticationService service;
+
+    public LoginController(AuthenticationService service) {
+        this.service = service;
+    }
 
     // http:localhost:8080/login?name=something
     @RequestMapping(value = "login", method = RequestMethod.GET)
@@ -18,9 +23,15 @@ public class LoginController {
     @RequestMapping(value = "login", method = RequestMethod.POST)
     public String welcomePage(@RequestParam String name,
                               @RequestParam String password,
-                              ModelMap model){
-        model.put("name", name);
-        model.put("password", password);
-        return "welcome";
+                              ModelMap model) {
+        if (service.authenticate(name, password)) {
+            model.put("name", name);
+            model.put("password", password);
+
+            // authentication
+            return "welcome";
+        }
+        model.put("error", "Invalid credentials");
+        return "login";
     }
 }
--- todo-management/src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+++ todo-management/src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
@@ -6,6 +6,7 @@
 <h1>Welcome to Login Page</h1>
 
 <form method="post">
+<pre>${error}</pre>
 Name: <input type="text" name="name" />
 password: <input type="password" name="password" />
 <input type="submit" />
