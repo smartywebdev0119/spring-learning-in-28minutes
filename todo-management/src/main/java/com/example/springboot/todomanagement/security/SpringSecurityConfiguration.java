69e50204050323bcf5679f1440351df7d9a985cc
smartywebdev0119 <smartwebdev0119@gmail.com>
2023-06-23 12:15:19 +0530

    Configuring spring security to get H2 console

--- todo-management/src/main/java/com/example/springboot/todomanagement/security/SpringSecurityConfiguration.java
+++ todo-management/src/main/java/com/example/springboot/todomanagement/security/SpringSecurityConfiguration.java
@@ -2,11 +2,15 @@ package com.example.springboot.todomanagement.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.function.Function;
 
@@ -41,6 +45,31 @@ public class SpringSecurityConfiguration {
                 .roles("USER", "ADMIN")
                 .build();
         return userDetails;
+    }
+
+
+    // All URLs are protected
+    // a login form is shown for unauthorized requests
+    // CSRF needs to disable
+    // Frames
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(
+                        authorization -> authorization.anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults()); // enable form login, all defaults that are associated with form login
+
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .headers(
+                        headers ->
+                                headers
+                                        .frameOptions(Customizer.withDefaults()).disable()
+                );
+
+        return http.build();
     }
 
 }
