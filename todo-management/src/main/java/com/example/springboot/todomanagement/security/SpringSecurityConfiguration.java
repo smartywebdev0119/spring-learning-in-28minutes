e5eb1ca2b023c52eb3a116b25d6d83547b2372f0
smartywebdev0119 <smartwebdev0119@gmail.com>
2023-06-23 08:02:24 +0530

    configuring spring security with custom username and password

---
+++ todo-management/src/main/java/com/example/springboot/todomanagement/security/SpringSecurityConfiguration.java
@@ -0,0 +1,40 @@
+package com.example.springboot.todomanagement.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import java.util.function.Function;
+
+@Configuration
+public class SpringSecurityConfiguration {
+
+    // LDAP OR Database
+    // InMemory
+
+    @Bean
+    public InMemoryUserDetailsManager createUserDetailsManager(){
+        Function<String, String> passwordEncoder = input -> passwordEncoder().encode(input);
+        UserDetails userDetails = User
+                .builder()
+                .passwordEncoder(
+                        passwordEncoder
+                )
+                .username("hemant")
+                .password("12345")
+                .roles("USER", "ADMIN")
+                .build();
+
+
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
