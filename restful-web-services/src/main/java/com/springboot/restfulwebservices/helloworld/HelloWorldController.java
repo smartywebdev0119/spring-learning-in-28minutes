518c80bf6f5f6c7f871e8712a88e3406fc34c025
smartywebdev0119 <smartwebdev0119@gmail.com>
2023-06-24 10:49:09 +0530

    Enhanced the hello world rest api to return a bean

--- restful-web-services/src/main/java/com/springboot/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/springboot/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,8 +1,6 @@
 package com.springboot.restfulwebservices.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +10,10 @@ public class HelloWorldController {
     @GetMapping("/hello-world")
     public String helloWorld(){
         return "Hello World!";
+    }
+
+    @GetMapping("/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello World");
     }
 }
