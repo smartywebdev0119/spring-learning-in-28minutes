a151522f339ca04a91d79caf65c7f7c324101bda
smartywebdev0119 <smartwebdev0119@gmail.com>
2023-06-24 08:44:42 +0530

    Created: First hello world rest api

---
+++ restful-web-services/src/main/java/com/springboot/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.springboot.restfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+//    @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
+    @GetMapping("/hello-world")
+    public String helloWorld(){
+        return "Hello World!";
+    }
+}
