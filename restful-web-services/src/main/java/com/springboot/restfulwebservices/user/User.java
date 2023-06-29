c0c11021a86ef011364c1e319688328d1ebe8cf7
smartywebdev0119 <smartwebdev0119@gmail.com>
2023-06-29 11:30:01 +0530

    Implemented static filtering for REST API

--- restful-web-services/src/main/java/com/springboot/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/springboot/restfulwebservices/user/User.java
@@ -1,5 +1,6 @@
 package com.springboot.restfulwebservices.user;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -8,8 +9,10 @@ import java.time.LocalDate;
 public class User {
     private Integer id;
     @Size(min = 2, message = "name should be atleast two characters")
+    @JsonProperty("user_name")
     private String name;
     @Past(message = "Date of Birth should be in past")
+    @JsonProperty("birth_date")
     private LocalDate birthDate;
 
     public User(Integer id, String name, LocalDate birthDate) {
