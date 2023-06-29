38844affae0d3b0434965b13f2ec818823a86d12
smartywebdev0119 <smartwebdev0119@gmail.com>
2023-06-29 12:48:31 +0530

    Added User details entity

--- restful-web-services/src/main/java/com/springboot/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/springboot/restfulwebservices/user/User.java
@@ -1,14 +1,20 @@
 package com.springboot.restfulwebservices.user;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+@Entity(name = "user_details")
 public class User {
+    @Id
+    @GeneratedValue
     private Integer id;
-    @Size(min = 2, message = "name should be atleast two characters")
+    @Size(min = 2, message = "name should be at least two characters")
     @JsonProperty("user_name")
     private String name;
     @Past(message = "Date of Birth should be in past")
---
+++ restful-web-services/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO user_details(id, birth_date, name)
+VALUES(100, current_date(), 'Hemant');
+INSERT INTO user_details(id, birth_date, name)
+VALUES(101, current_date(), 'sam');
+INSERT INTO user_details(id, birth_date, name)
+VALUES(102, current_date(), 'bob');
+INSERT INTO user_details(id, birth_date, name)
+VALUES(103, current_date(), 'Josh');
