1574b8ca656633b7008c857e6dcbb18d644650d0
smartywebdev0119 <smartwebdev0119@gmail.com>
2023-06-29 10:05:06 +0530

    Implemented HATEOAS for REST API

--- restful-web-services/src/main/java/com/springboot/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/springboot/restfulwebservices/user/UserResource.java
@@ -1,6 +1,10 @@
 package com.springboot.restfulwebservices.user;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
 import jakarta.validation.Valid;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -23,12 +27,15 @@ public class UserResource {
     }
 
     @GetMapping("/users/{id}")
-    public User retrieveUserById(@PathVariable Integer id) {
+    public EntityModel<User> retrieveUserById(@PathVariable Integer id) {
         User user = service.findOne(id);
         if(user == null) {
             throw new UserNotFoundException("id: " + id);
         }
-        return user;
+        EntityModel<User> entityModel = EntityModel.of(user);
+        WebMvcLinkBuilder link = linkTo(methodOn(this.getClass()).retrieveAllUsers());
+        entityModel.add(link.withRel("all-users"));
+        return entityModel;
     }
     @PostMapping("/users")
     public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
